cba4c8e3cd32ee819a5163a3796d951c96e2afcb
websiteverzekerd <mischa@websiteverzekerd.nl>
2016-11-04 19:12:44 +0100

    Files added

---
+++ be-secure-wordpress/be-secure-wordpress.php
@@ -0,0 +1,13 @@
+<?php
+/**
+* Plugin Name: Be Secure WordPress
+* Plugin URI: http://github.com/websiteverzekerd/be-secure-wordpress
+* Description:  WebsiteVerzekerd WordPress Plugin for automatic vulnerability scanning
+* Version: 1.0
+* Author: WebsiteVerzekerd
+* Author URI: www.websiteverzekerd.nl
+* License: GPLv2
+*/
+
+
+
