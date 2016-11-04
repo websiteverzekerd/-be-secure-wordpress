7048b11e4f081e297bc53871993490aff1b39053
websiteverzekerd <mischa@websiteverzekerd.nl>
2016-11-04 19:39:49 +0100

    New files added.

--- be-secure-wordpress/be-secure-wordpress.php
+++ be-secure-wordpress/be-secure-wordpress.php
@@ -9,5 +9,24 @@
 * License: GPLv2
 */
 
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+ 
+/** Step 1. */
+function be_secure_menu() {
+	add_menu_page( 'Be Secure', 'Be Secure', 'manage_options', 'be-secure-wordpress', 'be_secure_home', $icon_url = '', null); 
+}
+
+add_action( 'admin_menu', 'be_secure_menu' );
 
 
+
+/** Step 3. */
+function be_secure_home() {
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	}
+	echo '<div class="wrap">';
+	echo '<p>Here is where the form would go if I actually had options.</p>';
+	echo '</div>';
+}
